c84450a5b8ca6049f2f73567bf6c0bc770cf523f
ben casalino & samantha maturen <student@epicodus.com>
2015-08-05 13:51:11 -0700

    Migrated root route to Twig

--- app/app.php
+++ app/app.php
@@ -17,58 +17,7 @@
 
     $app->get("/", function() use ($app) {
 
-        $output = "";
-
-        $output .= "<!DOCTYPE html>
-            <html>
-            <head>
-            <title>To Do List</title>
-            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css'>
-            </head>
-            <body>
-            <div class='container'>
-        ";
-
-        if (!empty(Task::getAll())) {
-            $output .= "
-                <h1> To do list </h1>
-                <p>Here are all your tasks:</p>
-            ";
-
-            /* Loops through and displays array of all $task */
-            foreach (Task::getAll() as $task) {
-                $output .= "<p>" . $task->getDescription() . "</p>";
-            }
-
-        }
-
-        /* form for inserting description */
-        $output .= "
-            <form action='/tasks' method='post'>
-                <label for='description'>Task Description</label>
-                <input id='description' name='description' type='text'>
-
-                <button type='submit'>Add task</button>
-            </form>
-
-        ";
-
-
-        // will clear delete tasks
-        $output .= "
-            <form action='/delete_tasks' method='post'>
-                <button type ='submit'> delete </button>
-            </form>
-
-        ";
-
-        $output .= "
-            </div>
-            </body>
-            </html>
-        ";
-
-        return $app['twig']->render('tasks.html.twig');
+        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
 
     });
 
